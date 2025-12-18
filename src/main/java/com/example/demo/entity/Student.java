a37bc563ec1a42acd1d84ed2561e8b8da11d79bf
bala-amypo <bala@amypo.in>
2025-12-18 07:34:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,30 +1,31 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Column;
 @Entity
+// @Table(name = "students")
 public class Student {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message="Name filed cannot be empty")
+    @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
     private String name;
+    @Email(message="Invalid email id")
+    @Column(unique=true)
     private String email;
 
-    // Constructors
-    public Student() {
-    }
+    // @NotNull
+    // @Size(min,max)
+    // @Min
+    // @Max
+    // @Pattern(reg)
+    // @Postive
 
-    public Student(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    // Getters and Setters
     public Long getId() {
         return id;
     }
@@ -48,4 +49,15 @@ public class Student {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public Student(String name,String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Student() {
+    }
+    
+
+    
 }
