53250dedae70d0d3ff59139c66dbc21959e526f6
bala-amypo <bala@amypo.in>
2025-12-18 10:04:19 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,12 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Student;
+import com.example.demo.exception.ResourceNotFoundException;
 
 public interface StudentService {
 
     Student saveStudent(Student student);
-}
-public Student getStudentById(Long id){
+    public Student getStudentById(Long id){
         return studentRepository.findById(id)
         .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
     }
+}
