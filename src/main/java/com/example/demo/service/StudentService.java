e6ce7fe22b846ee06f2b99ab1ea67b8a5a8f4250
bala-amypo <bala@amypo.in>
2025-12-19 08:00:45 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,13 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Student;
-import com.example.demo.exception.ResourceNotFoundException;
 
 public interface StudentService {
 
     Student saveStudent(Student student);
-    public Student getStudentById(Long id){
-        return studentRepository.findById(id)
-        .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
-    }
+    Student getStudentById(Long id);
+    Student updatedata(Long id,Student student);
+    Student deletdata(Long id);
 }
--- src/main/java/com/example/demo/serviceImpl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/serviceImpl/StudentServiceImpl.java
@@ -1,19 +1,38 @@
-package com.example.demo.service;
+package com.example.demo.Serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+import com.example.demo.exception.ResourceNotFoundException;
 
 @Service
-public class StudentServiceImpl implements StudentService {
-
+public class StudentServiceImple implements StudentService {
     @Autowired
-    private StudentRepository repo;
+    StudentRepository studentRepository;
 
-    @Override
-    public Student saveStudent(Student student) {
-        return repo.save(student);
+    public Student saveStudent(Student student){
+        return studentRepository.save(student);
     }
+    
+    public Student getStudentById(Long id){
+        return studentRepository.findById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+    public Student updatedata(Long id,Student student){
+        Student exits=getStudentById(id);
+        exits.setName(student.getName());
+        exits.setEmail(student.getEmail());
+        return studentRepository.save(exits);
+        // .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+
+    public Student deletdata(Long id){
+        Student student=getStudentById(id);
+        studentRepository.delete(student);
+        return student;
+    }
+
 }
